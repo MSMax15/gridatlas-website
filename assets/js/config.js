663f13a65e874a5b6f43f9f3e7d570779d850715
FUZZY <107762834+MSMax15@users.noreply.github.com>
2026-07-20 23:16:51 +0200

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,2 +1,5 @@
-"use strict";
-window.GRIDATLAS_CONFIG=Object.freeze({formEndpoint:"https://formspree.io/f/xlgqnnel",contactEmail:"kontakt@gridatlas.de",siteUrl:"https://gridatlas.de"});
+window.GRIDATLAS_CONFIG = Object.freeze({
+  formEndpoint: "https://formspree.io/f/xlgqnnel",
+  contactEmail: "kontakt@gridatlas.de",
+  siteUrl: "https://gridatlas.de"
+});
